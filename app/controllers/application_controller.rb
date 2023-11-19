a22e20baf3193e70302b1222762f510db1ce17c2
reon0921 <reonshankou40@gmail.com>
2023-11-19 21:39:35 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -7,6 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
   end
 end
+
 
 private
 def prototypes_params
@@ -19,6 +21,7 @@ end
 
 def move_to_index
   unless user_signed_in?
-    redirect_to action: :index
+    
     end
   end
+  
